519996898b0edd5f44766bb0c7a5ec731d7ff2c5
Johannes Schulz <johannes@Johanness-MacBook-Air.local>
2025-03-16 13:35:44 +0530

    basic version is complete

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# In-memory task list (resets when server restarts)
+tasks = ['Walk the dogs', 'Cook Food', 'Study Maths']
+
+class Task(BaseModel):
+    task: str
+
+@app.get("/tasks")
+def get_tasks():
+    return {"tasks": tasks}
+
+@app.post("/tasks")
+def add_task(task: Task):
+    tasks.append(task.task)
+    return {"message": "Task added!"}
+
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
